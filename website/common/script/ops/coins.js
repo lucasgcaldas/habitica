52a7d6611e6b65c06c05c120cdecdca0eb0e36de
Lucas Gomes <88175144+lucasgcaldas@users.noreply.github.com>
2023-02-02 23:23:50 -0300

    atualizando funcao coin

--- website/common/script/ops/coins.js
+++ website/common/script/ops/coins.js
@@ -4,6 +4,7 @@ import BadRequest from '../libs/errors';
 export default function coins(user, type, quantity, req) {
     const { balance } = user;
     const coinValues = {
+        diamond: { value: 50, multiplier: 1.5, minBalance: 90 },
         gold: { value: 45, multiplier: 1.3, minBalance: 100 },
         silver: { value: 35, multiplier: 1.2, minBalance: 90 },
         bronze: { value: 25, multiplier: 1.1, minBalance: 80 },
